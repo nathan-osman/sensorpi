280614e59a6fac9e9be9bb34a9a54862d1b9649b
Nathan Osman <nathan@quickmediasolutions.com>
2024-12-29 03:52:55 -0800

    Add MQTT plugin.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/nathan-osman/sensorpi
 go 1.18
 
 require (
+	github.com/eclipse/paho.mqtt.golang v1.5.0
 	github.com/influxdata/influxdb-client-go/v2 v2.13.0
 	github.com/nathan-osman/go-sunrise v1.1.0
 	github.com/nathan-osman/nutclient/v3 v3.0.1
@@ -17,12 +18,14 @@ require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/google/uuid v1.3.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/oapi-codegen/runtime v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
---
+++ plugins/mqtt/mqtt.go
@@ -0,0 +1,65 @@
+package mqtt
+
+import (
+	"fmt"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/nathan-osman/sensorpi/plugin"
+	"gopkg.in/yaml.v3"
+)
+
+// Mqtt maintains a connection to an MQTT server
+type Mqtt struct {
+	client mqtt.Client
+}
+
+type outputConfig struct {
+	Addr string `yaml:"addr"`
+}
+
+type params struct {
+	Topic  string `yaml:"topic"`
+	Qos    uint8  `yaml:"qos"`
+	Retain bool   `yaml:"retain"`
+}
+
+func init() {
+	plugin.Register("mqtt", func(node *yaml.Node) (any, error) {
+		cfg := &outputConfig{}
+		if err := node.Decode(cfg); err != nil {
+			return nil, err
+		}
+		c := mqtt.NewClient(
+			mqtt.NewClientOptions().
+				AddBroker(fmt.Sprintf("tcp://%s", cfg.Addr)).
+				SetClientID("sensorpi").
+				SetKeepAlive(30 * time.Second),
+		)
+		if t := c.Connect(); t.Wait() && t.Error() != nil {
+			return nil, t.Error()
+		}
+		m := &Mqtt{
+			client: c,
+		}
+		return m, nil
+	})
+}
+
+func (m *Mqtt) Write(v float64, node *yaml.Node) error {
+	params := &params{
+		Qos:    1,
+		Retain: true,
+	}
+	if err := node.Decode(params); err != nil {
+		return err
+	}
+	if t := m.client.Publish(params.Topic, params.Qos, params.Retain, v); t.Wait() && t.Error() != nil {
+		return t.Error()
+	}
+	return nil
+}
+
+func (m *Mqtt) Close() {
+	m.client.Disconnect(1000)
+}
